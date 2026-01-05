3ca55ca4a975333d8f37a7f81e925404f3b3b8df
shotam27 <neshotam27@gmail.com>
2026-01-06 01:28:07 +0900

    Add missing getErrorMessage and getSuccessMessage functions

--- config.render.php
+++ config.render.php
@@ -86,3 +86,29 @@ function errorResponse($message, $statusCode = 400) {
 function successResponse($data, $message = 'Success') {
     jsonResponse(['success' => true, 'message' => $message, 'data' => $data]);
 }
+
+/**
+ * エラーメッセージを取得して削除
+ * @return string|null
+ */
+function getErrorMessage() {
+    if (isset($_SESSION['error_message'])) {
+        $message = $_SESSION['error_message'];
+        unset($_SESSION['error_message']);
+        return $message;
+    }
+    return null;
+}
+
+/**
+ * 成功メッセージを取得して削除
+ * @return string|null
+ */
+function getSuccessMessage() {
+    if (isset($_SESSION['success_message'])) {
+        $message = $_SESSION['success_message'];
+        unset($_SESSION['success_message']);
+        return $message;
+    }
+    return null;
+}
